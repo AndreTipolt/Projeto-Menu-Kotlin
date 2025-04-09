5b25e805934d4780e53b25c2a23fbb14c6f11c91
André Tipolt <andretipoltlopes@gmail.com>
2025-04-08 22:21:57 -0300

    Criando botão de sair

--- app/src/main/java/com/example/projetomenu/MainActivity.kt
+++ app/src/main/java/com/example/projetomenu/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.projetomenu
 
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
+import android.view.View
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -51,5 +53,9 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    fun onSair(item: MenuItem) {
+        finishAffinity()
     }
 }
